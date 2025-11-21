05d0afd376106e6b8887457e46719fa03641159a
Tytrez-Dixon <dixontytrez@gmail.com>
2025-11-21 14:28:41 -0500

    Update PilotRobot.py
    Added deadzone to combat roomba going rouge.

--- PilotRobot.py
+++ PilotRobot.py
@@ -175,29 +175,29 @@ while gamepad.isConnected():
         # Future work: Make robot speed proportional to how far the stick is pushed
 
         # SUGGESTION: FORMAT THESES ELIF STATEMENTS THE SAME WAY AS THE ELIF SATEMENTS FOR THE BUTTONS?
-        elif (control == 'RIGHT-Y') and (value == 0):
+        elif (control == 'RIGHT-Y') and (value <= 0.5 or value >= -0.5):
             bt7274.set_fwd_status(0)
             bt7274.set_back_status(0)
             bt7274.update_motion()
             print("Right joystick released")
-        elif (control == 'RIGHT-Y') and (value > 0):
+        elif (control == 'RIGHT-Y') and (value > 0.5):
             bt7274.set_back_status(1)
             bt7274.update_motion()
             print("Right joystick moved back")
-        elif (control == 'RIGHT-Y') and (value < 0):
+        elif (control == 'RIGHT-Y') and (value < -0.5):
             bt7274.set_fwd_status(1)
             bt7274.update_motion()
             print("Right joystick moved forward")
-        elif (control == 'LEFT-X') and (value == 0):
+        elif (control == 'LEFT-X') and (value <= 0.5 or value >= -0.5):
             bt7274.set_right_status(0)
             bt7274.set_left_status(0)
             bt7274.update_motion()
             print("Left joystick released")
-        elif (control == 'LEFT-X') and (value > 0):
+        elif (control == 'LEFT-X') and (value > 0.5):
             bt7274.set_right_status(1)
             bt7274.update_motion()
             print("Left joystick moved right")
-        elif (control == 'LEFT-X') and (value < 0):
+        elif (control == 'LEFT-X') and (value < -0.5):
             bt7274.set_left_status(1)
             bt7274.update_motion()
             print("Left joystick moved left")
