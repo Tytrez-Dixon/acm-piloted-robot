14d20562488b49b477c79c6f69d281e894ace7c6
Tytrez-Dixon <dixontytrez@gmail.com>
2025-11-24 11:53:48 -0500

    Update PilotRobot.py
    Fixed a logic issue.

--- PilotRobot.py
+++ PilotRobot.py
@@ -176,7 +176,7 @@ while gamepad.isConnected():
 
         # SUGGESTION: FORMAT THESES ELIF STATEMENTS THE SAME WAY AS THE ELIF SATEMENTS FOR THE BUTTONS?
         # Add dead zone for right joystick to account for snapback and stick-drift.
-        elif (control == 'RIGHT-Y') and (value <= 0.25 or value >= -0.25):
+        elif (control == 'RIGHT-Y') and (value <= 0.25 and value >= -0.25):
             bt7274.set_fwd_status(0)
             bt7274.set_back_status(0)
             bt7274.update_motion()
