a38b8904a3bfde542c9d6795fc26e77c3db0773d
Tytrez-Dixon <dixontytrez@gmail.com>
2025-11-24 13:08:27 -0500

    Update PilotedRobot.py
    Adjusted default speed to 300mm/s

--- PilotedRobot.py
+++ PilotedRobot.py
@@ -146,7 +146,9 @@ class PilotedRobot:
         # This function adjusts the movement of the robot, after the status register has been updated.
         # For example, to make the robot go forward, you would use set_fwd_status(true) then update_motion()
 
-        # Velocity  = 200mm/s (this is the same speed we use elsewhere, assume turning velocity is the same as straight velocity)
+        # Velocity  = ____mm/s (this is the same speed we use elsewhere, assume turning velocity is the same as straight velocity)
+
+        # THIS INFO IS OUTDATED! VELOCITY HAS BEEN CHANGED TO ______mm/s!
         # T = 15s (this means the robot should complete a full circle every 15 seconds)
         # Turn radius = 478mm (Derived from the formula v=(2*pi*r)/T )
         # This translates to \x01\xDE or \xFE\x22
@@ -163,33 +165,33 @@ class PilotedRobot:
             case 0:
                 self.drive(b'\x00', b'\x00', b'\x00', b'\x00') # Stop
             case 1:
-                self.drive(b'\x01', b'\xF4', b'\xFF', b'\xFF') # Turn Right (Clockwise) in Place
+                self.drive(b'\x01', b'\xFE', b'\xFF', b'\xFF') # Turn Right (Clockwise) in Place
             case 2:
-                self.drive(b'\xFE', b'\x0C', b'\x00', b'\x00') # Backwards
+                self.drive(b'\xFE', b'\xD4', b'\x00', b'\x00') # Backwards
             case 3:
-                self.drive(b'\xFE', b'\x0C', b'\xFE', b'\x22') # Backwards While Veering Right
+                self.drive(b'\xFE', b'\xD4', b'\xFE', b'\x22') # Backwards While Veering Right
             case 4:
-                self.drive(b'\x01', b'\xF4', b'\x00', b'\x01') # Turn Left (Counterclockwise) in Place
+                self.drive(b'\x01', b'\xFE', b'\x00', b'\x01') # Turn Left (Counterclockwise) in Place
             case 5:
                 self.drive(b'\x00', b'\x00', b'\x00', b'\x00') # Stop (No Rotation)
             case 6:
-                self.drive(b'\xFE', b'\x0C', b'\x01', b'\xDE') # Backwards While Veering Left
+                self.drive(b'\xFE', b'\xD4', b'\x01', b'\xDE') # Backwards While Veering Left
             case 7:
-                self.drive(b'\xFE', b'\x0C', b'\x00', b'\x00') # Backwards
+                self.drive(b'\xFE', b'\xD4', b'\x00', b'\x00') # Backwards
             case 8:
-                self.drive(b'\x01', b'\xF4', b'\x00', b'\x00') # Forward
+                self.drive(b'\x01', b'\xFE', b'\x00', b'\x00') # Forward
             case 9:
-                self.drive(b'\x01', b'\xF4', b'\xFE', b'\x22') # Forward While Veering Right
+                self.drive(b'\x01', b'\xFE', b'\xFE', b'\x22') # Forward While Veering Right
             case 10:
                 self.drive(b'\x00', b'\x00', b'\x00', b'\x00') # Stop
             case 11:
-                self.drive(b'\x01', b'\xF4', b'\xFF', b'\xFF') # Turn Right (Clockwise) in Place
+                self.drive(b'\x01', b'\xFE', b'\xFF', b'\xFF') # Turn Right (Clockwise) in Place
             case 12:
-                self.drive(b'\x01', b'\xF4', b'\x01', b'\xDE') # Forward While Veering Left
+                self.drive(b'\x01', b'\xFE', b'\x01', b'\xDE') # Forward While Veering Left
             case 13:
-                self.drive(b'\x01', b'\xF4', b'\x00', b'\x00') # Forward
+                self.drive(b'\x01', b'\xFE', b'\x00', b'\x00') # Forward
             case 14:
-                self.drive(b'\x01', b'\xF4', b'\x00', b'\x01') # Turn Left (Counterclockwise) in Place
+                self.drive(b'\x01', b'\xFE', b'\x00', b'\x01') # Turn Left (Counterclockwise) in Place
             case 15:
                 self.drive(b'\x00', b'\x00', b'\x00', b'\x00') # Stop
             case _:
